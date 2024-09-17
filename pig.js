3f504c739760fe0e0de8a87cbe7709a1f2bab0d2
Noah1052 <edison_90@outlook.com>
2024-09-18 02:17:49 +0800

    1.3

--- pig.js
+++ pig.js
@@ -240,20 +240,20 @@ document.addEventListener('keyup', event => {
     }
 });
 
-document.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-        event.preventDefault();
-    }
-}, {passive: false});
-
-let lastTouchEnd = 0;
-document.addEventListener('touchend', function (event) {
-    const now = (new Date()).getTime();
-    if (now - lastTouchEnd <= 100) {
-        event.preventDefault();
-    }
-    lastTouchEnd = now;
-}, false);
+// document.addEventListener('touchstart', function (event) {
+//     if (event.touches.length > 1) {
+//         event.preventDefault();
+//     }
+// }, {passive: false});
+//
+// let lastTouchEnd = 0;
+// document.addEventListener('touchend', function (event) {
+//     const now = (new Date()).getTime();
+//     if (now - lastTouchEnd <= 100) {
+//         event.preventDefault();
+//     }
+//     lastTouchEnd = now;
+// }, false);
 
 function update(time = 0) {
     const deltaTime = time - lastTime;
